a5512fa674e520a25ec59f90fceec6135af0a140
kron0zzz <alanandrey234@gmail.com>
2025-11-27 09:51:16 -0500

    nose

---
+++ Api_final/Controllers/ReservaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Api_final.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_final.Controllers
+{
+    [ApiController]
+    [Route("api / [controller] ")]
+    public class ReservaController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public ReservaController(ApiContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var reservas = await _context.Reservas.ToListAsync();
+            return Ok(reservas);
+        }
+    }
+}
+
+
+
+   
+
+
+
+
